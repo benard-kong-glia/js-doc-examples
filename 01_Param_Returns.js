9aca705ce9535190d0d4764e806c326934c0cbe6
Benard Kong <benard.kong@glia.com>
2023-12-29 17:04:39 -0500

    Examples and tsconfig.json

---
+++ 01_Param_Returns.js
@@ -0,0 +1,19 @@
+function identifyAnimal1(photo) {
+  // Do somethiing
+}
+
+const animal1 =
+  /** What does the return represent? A category, or a biological species? */
+  identifyAnimal1(/** I don't know what to include here - is it a blob, a URL, or string? */);
+
+/**
+ * @param {Blob} photo Include description here if it is not obvious. Prefer good naming.
+ * @returns {string} The animal category
+ */
+function identifyAnimal2(photo) {
+  // Do somethiing
+}
+
+const animal2 = identifyAnimal2("blahblah");
+
+export {};
