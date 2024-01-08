85baca5ab7995bb5e09e3060808d8a5123130984
Benard Kong <benard.kong@glia.com>
2024-01-08 16:10:36 -0500

    Typedef, type, and property examples

---
+++ 03_typedef.js
@@ -0,0 +1,64 @@
+// @ts-check
+
+/**
+ * @typedef {Object} Student
+ * @property {string} name
+ * @property {number} age 0 - 120, integers only
+ * @property {string} major
+ * @property {number} gpa 0.0-4.0
+ * @property {string} filler001
+ * @property {number} filler002
+ * @property {number} filler003
+ * @property {string} filler004
+ * @property {number} filler005
+ * @property {number} filler006
+ * @property {string} filler007
+ * @property {number} filler008
+ * @property {number} filler009
+ * @property {boolean} filler010
+ * @property {number} filler011
+ * @property {string} filler012
+ * @property {number} filler013
+ * @property {number} filler014
+ * @property {string} filler015
+ * @property {number} filler016
+ * @property {boolean} filler017
+ * @property {number} filler018
+ * @property {number} filler019
+ * @property {boolean} filler020
+ */
+
+// TODO: Fill in the missing properties
+/** @type {Student} */
+const james = {
+  name: "James",
+  age: 25,
+  major: "English",
+  gpa: 3.5,
+  filler005: 0,
+  filler007: "foobar004",
+  filler014: 0,
+  filler002: 0,
+  filler006: 0,
+  filler010: false,
+  filler003: 0,
+  filler012: "foobar003",
+  filler019: 0,
+  filler016: 0,
+  filler020: true,
+  filler011: 0,
+  filler013: 0,
+  filler015: "foobar002",
+  filler009: 0,
+  filler004: "foobar001",
+  filler017: false,
+  filler018: 0,
+};
+
+/**
+ * @typedef {{
+ *   name: string,
+ *   teachers: string[],
+ *   students: Student[],
+ * }} School
+ */
